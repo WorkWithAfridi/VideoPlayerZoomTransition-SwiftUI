2abfdfa346cc1a7a8d769b55ecbc1b4ec00c7acf
Khondakar Afridi <afridi.khondakar@gmail.com>
2024-12-27 21:09:24 +0600

    Initial Commit

---
+++ ZoomTransitions/ZoomTransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomTransitionsApp.swift
+//  ZoomTransitions
+//
+//  Created by Khondakar Afridi on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ZoomTransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
